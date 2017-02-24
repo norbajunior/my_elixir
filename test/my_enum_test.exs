b1e802672502dd970d9a8bd6abfc18019da1bd05
Norberto Oliveira Junior <norbejunior@gmail.com>
2017-02-24 19:01:48 -0300

    Add MyEnum module

---
+++ test/my_enum_test.exs
@@ -0,0 +1,23 @@
+defmodule MyEnumTest do
+  use ExUnit.Case
+  doctest MyEnum
+
+  setup_all do
+    { :ok, [ list: [4, 5, 6, 7, 8] ] }
+  end
+
+  test "#all? elements are greater than 3", %{ list: list } do
+    assert MyEnum.all?(list, &(&1 > 3))
+    assert MyEnum.all?([], &(&1 > 3)) # the same behaviour of `Enum.all/2`
+  end
+
+  test "#filter numbers greater than 5", %{ list: list } do
+    assert MyEnum.filter(list, &(&1 > 5)), [6, 7, 8]
+    assert MyEnum.filter([], &(&1 > 5)), []
+  end
+
+  test "#each function", %{ list: list } do
+    !assert MyEnum.each(list, &(&1)), []
+    assert MyEnum.each([], &(&1)), []
+  end
+end
