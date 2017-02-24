186ac8e04f5cb740130a8b486572070e8feabb2a
Norberto Oliveira Junior <norbejunior@gmail.com>
2017-02-24 19:58:26 -0300

    Change test descriptions

--- test/my_enum_test.exs
+++ test/my_enum_test.exs
@@ -6,17 +6,17 @@ defmodule MyEnumTest do
     { :ok, [ list: [4, 5, 6, 7, 8] ] }
   end
 
-  test "#all? elements are greater than 3", %{ list: list } do
+  test "#all? returns true if the given `func` evaluates to true on any of the list items", %{ list: list } do
     assert MyEnum.all?(list, &(&1 > 3))
     assert MyEnum.all?([], &(&1 > 3)) # the same behaviour of `Enum.all/2`
   end
 
-  test "#filter numbers greater than 5", %{ list: list } do
+  test "#filter returns only those elements for which `func` returns a truthy value.", %{ list: list } do
     assert MyEnum.filter(list, &(&1 > 5)), [6, 7, 8]
     assert MyEnum.filter([], &(&1 > 5)), []
   end
 
-  test "#each that iterates trough the elements list", %{ list: list } do
+  test "#each invokes the given `func` for each item in the enumerable.", %{ list: list } do
     !assert MyEnum.each(list, &(&1)), []
     assert MyEnum.each([], &(&1)), []
   end
