acf68ab0d72aaeafbdbf4aeb281fbd5f0d7a86c2
Norberto Oliveira Junior <norbejunior@gmail.com>
2017-02-27 14:36:55 -0300

    Improve the readability of MyEnum.split/2

--- lib/my_enum.ex
+++ lib/my_enum.ex
@@ -20,15 +20,15 @@ defmodule MyEnum do
   end
 
   def split([], _), do: { [], [] }
-  def split(list, count) when count < 0 do
-    _split(list, [], length(list) - -(count))
+  def split(list, at) when at < 0 do
+    _split(list, [], length(list) - -(at))
   end
-  def split(list, count), do: _split(list, [], count)
+  def split(list, at), do: _split(list, [], at)
 
   def _split([], left, _),    do: { reverse(left), [] }
   def _split(right, left, 0), do: { reverse(left), right }
-  def _split([ head | tail ], left, count) do
-    _split(tail, [ head | left ], count-1)
+  def _split([ head | tail ], left, at) do
+    _split(tail, [ head | left ], at-1)
   end
 
   def reverse([]), do: []
