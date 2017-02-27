0557276435c9d2410b6739acdefd56f2d18cc821
Norberto Oliveira Junior <norbejunior@gmail.com>
2017-02-27 14:10:59 -0300

    Refactor tests
    - Organize them with describe macros

--- test/my_enum_test.exs
+++ test/my_enum_test.exs
@@ -8,29 +8,48 @@ defmodule MyEnumTest do
     { :ok, [ list: [4, 5, 6, 7, 8] ] }
   end
 
-  test "#all? returns true if the given `func` evaluates to true on any of the list items", %{ list: list } do
-    assert MyEnum.all?(list, &(&1 > 3))
-    assert MyEnum.all?([], &(&1 > 3)) # the same behaviour of `Enum.all/2`
-  end
-
-  test "#filter returns only those elements for which `func` returns a truthy value.", %{ list: list } do
-    assert MyEnum.filter(list, &(&1 > 5)) == [6, 7, 8]
-    assert MyEnum.filter([], &(&1 > 5)) == []
-  end
-
-  test "#each invokes the given `func` for each item in the enumerable.", %{ list: list } do
-    func = fn ->
-      assert MyEnum.each(list, &IO.puts/1) == :ok
+  describe "MyEnum.all?/2" do
+    test "returns true if the given `func` evaluates to true on any of the list items", %{ list: list } do
+      assert MyEnum.all?(list, &(&1 > 3))
     end
 
-    assert capture_io(func) == "4\n5\n6\n7\n8\n"
+    test "returns true when is given an empty list" do
+      assert MyEnum.all?([], &(&1 > 3)) # the same behaviour of `Enum.all/2`
+    end
+  end
+
+
+  describe "MyEnum.filter/2" do
+    test "returns only those elements for which `func` returns a truthy value.", %{ list: list } do
+      assert MyEnum.filter(list, &(&1 > 5)) == [6, 7, 8]
+    end
+
+    test "returns an empty list in case is given an empty list" do
+      assert MyEnum.filter([], &(&1 > 5)) == []
+    end
+  end
+
+  describe "MyEnum.each/2" do
+    test "invokes the given `func` for each item in the enumerable.", %{ list: list } do
+      func = fn ->
+        assert MyEnum.each(list, &IO.puts/1) == :ok
+      end
+
+      assert capture_io(func) == "4\n5\n6\n7\n8\n"
+    end
   end
 
   describe "MyEnum.split/2" do
     test "splits in two a list leaving `count` elements in the first one", %{ list: list } do
       assert MyEnum.split(list, 3) == { [4, 5, 6], [7, 8] }
-      assert MyEnum.split([], 3) == { [], [] }
       assert MyEnum.split(list, -2) == { [4, 5, 6], [7, 8] }
+    end
+
+    test "returns a tuple with two empty lists when is given an empty list" do
+      assert MyEnum.split([], 3) == { [], [] }
+    end
+
+    test "returns a tuple with an empty list as the first one when a negative count is greather than list length" do
       # TODO: assert MyEnum.split(list, -6) == { [], [4, 5, 6, 7, 8] }
     end
   end
