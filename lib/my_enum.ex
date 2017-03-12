f32e578f0a087881a7b7b0b88f20dfc19bb51ccf
Norberto Oliveira Junior <norbejunior@gmail.com>
2017-03-12 11:54:54 -0300

    Refactor lib/my_enum.ex improving its readability

--- lib/my_enum.ex
+++ lib/my_enum.ex
@@ -2,15 +2,24 @@ defmodule MyEnum do
   def all?([], _), do: true
   def all?([head | tail], func), do: func.(head) && all?(tail, func)
 
-  def each([], _), do: :ok
-  def each([head], func), do: func.(head)
+  def each([], _) do
+    :ok
+  end
+
+  def each([head], func) do
+    func.(head)
+  end
+
   def each([head | tail], func) do
     func.(head)
 
     each tail, func
   end
 
-  def filter([], _), do: []
+  def filter([], _) do
+    []
+  end
+
   def filter([head | tail], func) do
     if func.(head) do
       [ head | filter(tail, func) ]
@@ -19,16 +28,22 @@ defmodule MyEnum do
     end
   end
 
-  def split([], _), do: { [], [] }
-  def split(list, at) when at < 0 do
-    _split(list, [], length(list) - -(at))
+  def split([], _) do
+    { [], [] }
   end
-  def split(list, at), do: _split(list, [], at)
 
-  defp _split([], left, _),    do: { reverse(left), [] }
-  defp _split(right, left, 0), do: { reverse(left), right }
-  defp _split([ head | tail ], left, at) do
-    _split(tail, [ head | left ], at-1)
+  def split(list, at) when at < 0 do
+    do_split(list, [], length(list) - -(at))
+  end
+
+  def split(list, at) do
+    do_split(list, [], at)
+  end
+
+  defp do_split([], left, _),    do: { reverse(left), [] }
+  defp do_split(right, left, 0), do: { reverse(left), right }
+  defp do_split([ head | tail ], left, at) do
+    do_split(tail, [ head | left ], at-1)
   end
 
   def take([], _), do: []
@@ -36,17 +51,23 @@ defmodule MyEnum do
   def take([ head | tail ], at), do: [ head | take(tail, at-1) ]
 
   def reverse([]), do: []
-  def reverse([ _head | _tail ] = list), do: _reverse(list, [])
+  def reverse([ _head | _tail ] = list), do: do_reverse(list, [])
 
-  defp _reverse([], new), do: new
-  defp _reverse([ head | tail ], new), do: _reverse(tail, [ head | new ])
+  defp do_reverse([], new), do: new
+  defp do_reverse([ head | tail ], new), do: do_reverse(tail, [ head | new ])
 
   def flatten([]), do: []
-  def flatten([_ | _] = list), do: _flatten(list, [])
+  def flatten([_ | _] = list), do: do_flatten(list, [])
 
-  defp _flatten([], flattened_list), do: flattened_list
-  defp _flatten([head | tail], flattened_list) do
-    _flatten(head, _flatten(tail, flattened_list))
+  defp do_flatten([], flattened_list) do
+    flattened_list
   end
-  defp _flatten(x, flattened_list), do: [ x | flattened_list ]
+
+  defp do_flatten([head | tail], flattened_list) do
+    do_flatten head, do_flatten(tail, flattened_list)
+  end
+
+  defp do_flatten(x, flattened_list) do
+    [ x | flattened_list ]
+  end
 end
